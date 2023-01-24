2e2ddb6dea71d696bd87cebf1a0601721b5b078f
Tobias Melen <tobias.melen@gmail.com>
2023-01-24 22:17:09 +0100

    Simplify test and add logging for debugging

--- test.js
+++ test.js
@@ -1,36 +1,35 @@
 //@ts-check
 const { test } = require("node:test");
 const { spawn } = require("node:child_process");
-const path = require("node:path");
 
 test(
   "Test dev server startup",
   { timeout: 10_000 },
-  () =>
-    new Promise((res, rej) =>
-      setImmediate(() => {
-        const childProcess = spawn("pnpm", ["run", "dev"], {
-          cwd: "sandbox",
-          shell: true,
-          stdio: "pipe",
-        });
-        childProcess.stdout.setEncoding("utf-8");
-        childProcess.stderr.setEncoding("utf-8");
-        childProcess.stdout.on("data", (msg) => {
-          if (msg.includes("compiled client and server successfully")) {
-            childProcess.kill("SIGINT");
-            res("Completed");
-          }
-        });
-        childProcess.stderr.on("data", (msg) => {
-          rej(Error(msg));
-        });
-        childProcess.on("error", (err) => {
-          rej(err);
-        });
-        childProcess.on("close", (exitCode) => {
-          rej(`Exit code ${exitCode}`);
-        });
-      })
-    )
+  async () =>
+    await new Promise((res, rej) => {
+      console.log("Spawining process");
+      const childProcess = spawn("pnpm", ["run", "dev"], {
+        cwd: "sandbox",
+        shell: true,
+        stdio: "pipe",
+      });
+      childProcess.stdout.setEncoding("utf-8");
+      childProcess.stderr.setEncoding("utf-8");
+      childProcess.stdout.on("data", (msg) => {
+        console.log("Received message", msg);
+        if (msg.includes("compiled client and server successfully")) {
+          res("Completed");
+          childProcess.kill("SIGINT");
+        }
+      });
+      childProcess.stderr.on("data", (msg) => {
+        rej(Error(msg));
+      });
+      childProcess.on("error", (err) => {
+        rej(err);
+      });
+      childProcess.on("close", (exitCode) => {
+        rej(`Exit code ${exitCode}`);
+      });
+    })
 );
